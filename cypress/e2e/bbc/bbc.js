f3a5d15dfa0bd52d13e5ef9a20d7cfedd4a74508
thatianne@psagotequity.co.il <amanda_fusuma@hotmail.com>
2023-08-16 23:03:09 +0100

    Another way to resolve log print scenario 1

--- cypress/e2e/bbc/bbc.js
+++ cypress/e2e/bbc/bbc.js
@@ -13,20 +13,20 @@ Given("I am on empty home page", () => {
 
 When("display the name of all today's teams", () => {
   cy.visit('/sport')
-  cy.get('[href="https://www.bbc.com/sport"]').contains('Sport').click({force: true} )
+  cy.get('[href="https://www.bbc.com/sport"]').contains('Sport').click({force: true})
   cy.get('[href="/sport/football"]').contains('Football').click()
   cy.get('[href="/sport/football/scores-fixtures"]').contains('Scores & Fixtures').click()
 });
 
 Then("I validate if it is bringing a name or not", () => {
-  const getText = ($el) => {
-    return Cypress._.map($el, 'innerText')
-  }
-  if (cy.get('span[class="sp-c-fixture__team-name-wrap"]').should("be.visible")) {
-      console.log(getText)
-  } else {
-    console.log('Not found')
-  } 
+  cy.get('span[class="sp-c-fixture__team-name-wrap"]').should("be.visible").then(($teams) => {
+    if ($teams.length > 0) {
+      const teamNames = $teams.map((index, el) => Cypress.$(el).text()).get();
+      cy.log('All teams:', teamNames.join(', '));
+    } else {
+      cy.log('Not found.');
+    }
+  })
 });
 
 //SCENARIO 2
